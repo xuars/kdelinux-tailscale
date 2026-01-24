90925c875df036bb4f87bbdb8fbb8173342cf9d4
Yago Raña Gayoso <yago.rana.gayoso@gmail.com>
2026-01-24 17:43:23 +0100

    update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,7 @@
 sudo systemctl stop tailscaled
 sudo systemctl disable tailscaled
 sudo rm /etc/systemd/system/tailscaled.service
+sudo rm -rf /etc/systemd/system/tailscaled.service.d
 sudo rm /etc/default/tailscaled
 sudo rm /etc/profile.d/tailscale.sh
 sudo rm -rf /opt/tailscale/tailscale
